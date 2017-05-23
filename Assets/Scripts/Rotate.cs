fd52211cb4b539d6412407851a230e6fde0ade72
Delainey Ackerman <dack91@gmail.com>
2017-05-23 11:19:28 +0100

    init test repo

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    public GameObject obj;
+    private Rigidbody rb;
+    private Transform trans;
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        trans = obj.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        trans.Rotate(transform.up * Time.deltaTime * speed);
+	}
+}
